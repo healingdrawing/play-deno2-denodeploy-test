d3568f3dfe94f34253fb177796c6e9a56ec4821b
healingdrawing <healingdrawing@gmail.com>
2024-10-06 09:22:49 +0300

    super simple test to know deno2 is not dead for deploy

--- README.md
+++
@@ -1,4 +0,0 @@
-Start from `deno init .` then modify using hono.  
-
-The `.env` file used locally, not uploaded into repo.  
-Environment variables must be added into deploy.  
--- main.ts
+++ main.ts
@@ -1,11 +1,4 @@
-import {csrf, cors, BACKHOST, FRONTHOST, Hono, home} from "./deps.ts"
-
-
-const app = new Hono()
-app.use(csrf({ origin: [BACKHOST, FRONTHOST], }))
-app.use(cors({ origin: [BACKHOST, FRONTHOST], }))
-
-app.route('/', home)
-
-// Deno.listen({ port:8000}) //todo: check later, itis not clear vs frontend
-Deno.serve(app.fetch);
+function handler(_req: Request): Response {
+  return new Response("Hello, World!");
+}
+Deno.serve(handler);
